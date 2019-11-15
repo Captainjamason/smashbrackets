a733be44ffee0fde7dd392bbf640c66a9ffcbca1
Jamason Davis <jpdavis2471@gmail.com>
2019-11-15 16:20:17 -0600

    Working user creation (i hope)

---
+++ api/createuser.php
@@ -0,0 +1,34 @@
+<?php
+header("Access-Control-Allow-Origin: http://localhost/api");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+include_once 'config/database.php';
+include_once 'objects/user.php';
+
+$database = new Database();
+$db = $database->getConnection();
+
+$user = new User($db);
+
+$data = json_decode(file_get_contents("php://input"));
+
+$user->uname = $data->uname;
+$user->pass = $data->pass;
+$user->money = "500";
+
+if(
+	!empty($user->uname) &&
+	!empty($user->pass) &&
+	!empty($user->money) &&
+	$user->create()
+) {
+	http_response_code(200);
+	echo json_encode(array("message" => "User was created"));
+} else {
+	http_response_code(400);
+	echo json_encode(array("message" => "User could not be created"));
+}
+?>
--- api/objects/user.php
+++ api/objects/user.php
@@ -10,5 +10,25 @@ class User {
 	public function __construct($db) {
 		$this->conn = $db;
 	}
+
+	function create() {
+		$query = "INSERT INTO " . $this->table_name . " SET uname = :uname, pass = :pass, money = :money";
+		$stmt = $this->conn->prepare($query);
+
+		$this->uname=htmlspecialchars(strip_tags($this->uname));
+		$this->pass=htmlspecialchars(strip_tags($this->pass));
+		$this->money=htmlspecialchars(strip_tags($this->money));
+
+		$stmt->bindParam(':uname', $this->uname);
+		$password_hash = password_hash($this->pass, PASSWORD_DEFAULT);
+		$stmt->bindParam(':password', $password_hash);
+		$stmt->bindParam(':money', $this->money);
+
+		if($stmt->execute()) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
 ?>
